e9e6af8816a50e6e15e5b2672bb63edeca2beb6e
Faraifarrell <faraifarouk@gmail.com>
2024-05-23 22:51:44 +0200

    update with brackets

--- VPC.tf
+++ VPC.tf
@@ -119,4 +119,5 @@ resource "aws_route_table_association" "Private_Subnet2_Asso" {
   route_table_id = aws_route_table.RB_Private_RouteTable.id
   subnet_id      = aws_subnet.private-2.id
   depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-2]
-i}
+}
+
