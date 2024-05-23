d34567df34dea7e4d13f7abd4c018080eac9af2f
Faraifarrell <faraifarouk@gmail.com>
2024-05-23 23:07:40 +0200

    update123

--- VPC.tf
+++ VPC.tf
@@ -78,7 +78,7 @@ resource "aws_route_table" "RB_Public_RouteTable" {
   vpc_id = aws_vpc.dev_vpc.id
 
   route {
-    cidr_block = var.CIDR_BLOCK
+    cidr_block = 10.0.0.0/16
     gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
