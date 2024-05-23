b1eb2ef174be0736e98795ec544f56ec1449e548
Faraifarrell <faraifarouk@gmail.com>
2024-05-23 22:39:37 +0200

    Ip CIDR update

--- VPC.tf
+++ VPC.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "dev_vpc" {
 
 resource "aws_subnet" "public-1" {
   vpc_id     = aws_vpc.dev_vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.1.0/16"
   availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 
@@ -22,7 +22,7 @@ resource "aws_subnet" "public-1" {
 
 resource "aws_subnet" "private-1" {
   vpc_id     = aws_vpc.dev_vpc.id 
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.2.0/16"
   availability_zone = "us-east-1a"
 
   tags = {
@@ -32,7 +32,7 @@ resource "aws_subnet" "private-1" {
 
 resource "aws_subnet" "public-2" {
   vpc_id     = aws_vpc.dev_vpc.id
-  cidr_block = "10.0.3.0/24"
+  cidr_block = "10.0.3.0/16"
   availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
 
@@ -43,7 +43,7 @@ resource "aws_subnet" "public-2" {
 
 resource "aws_subnet" "private-2" {
   vpc_id     = aws_vpc.dev_vpc.id 
-  cidr_block = "10.0.4.0/24"
+  cidr_block = "10.0.4.0/16"
   availability_zone = "us-east-1b"
 
   tags = {
@@ -119,4 +119,4 @@ resource "aws_route_table_association" "Private_Subnet2_Asso" {
   route_table_id = aws_route_table.RB_Private_RouteTable.id
   subnet_id      = aws_subnet.private-2.id
   depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-2]
-}
+i}
